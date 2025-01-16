44f882279d2020dd5f8291dec938268999d6e10f
istywhyerlina <istywahyuerlina@gmail.com>
2025-01-16 20:27:42 +0700

    unit_test in local

--- src/api.py
+++ src/api.py
@@ -3,32 +3,43 @@ import mlflow
 import dotenv
 import uvicorn
 import numpy as np
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-dotenv.load_dotenv(".env")
+import json
+dotenv.load_dotenv("../.env")
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 CHOSEN_MODEL = os.getenv("MODEL_ALIAS")
 mlflow.set_tracking_uri(uri = MLFLOW_TRACKING_URI)
 mlflow.set_experiment("Churn Experiment")
-model = mlflow.pyfunc.load_model(f"models:/RF best param@{CHOSEN_MODEL}")
+model = mlflow.pyfunc.load_model(f"models:/RandomForest@{CHOSEN_MODEL}")
 class api_data(BaseModel):
-    x1 : float
-    x2 : float
-    x3 : float
-    x4 : float
-    x5 : float
-    x6 : float
-    x7 : float
-    x8 : float
-    x9 : float
-    x10 : float
+    CreditScore : float
+    Geography : float
+    Gender : float
+    Age : float
+    Tenure : float
+    Balance : float
+    NumOfProducts : float
+    HasCrCard : float
+    IsActiveMember : float
+    EstimatedSalary : float
 app = FastAPI()
 @app.get("/")
 def home():
 	    return "Hello, FastAPI up!"
+'''@app.get("/predict/")
+def predict(data: api_data):
+    #data = np.array([[data.x1, data.x2, data.x3, data.x4,data.x5,data.x6,data.x7, data.x8,data.x9,data.x10]]).astype(np.double)
+    y_pred = int(model.predict(data)[0])
+    return {"res" : y_pred, "error_msg": ""}'''
 @app.post("/predict/")
 def predict(data: api_data):
-    data = np.array([[data.x1, data.x2, data.x3, data.x4,data.x5,data.x6,data.x7, data.x8,data.x9,data.x10]]).astype(np.float64)
+    #data = np.array([[data["x1"], data['x2'], data['x3'], data['x4'],data['x5'],data['x6'],data['x7'], data['x8'],data['x9'],data['x10']]]).astype(np.float64)
+    #data = pd.DataFrame({'CreditScore': data.CreditScore, 'Geography': data.Geography, 'Gender': data.Gender, 'Age': data.Age, 'Tenure': data.Tenure, 'Balance': data.Balance, 'NumOfProducts': data.NumOfProducts, 'HasCrCard': data.HasCrCard, 'IsActiveMember': data.IsActiveMember, 'EstimatedSalary': data.EstimatedSalary}) 
+    data = np.array([[data.CreditScore, data.Geography, data.Gender, data.Age,data.Tenure,data.Balance,data.NumOfProducts, data.HasCrCard,data.IsActiveMember,data.EstimatedSalary]])
+    #print(type(data))
+    #print(data)
     y_pred = int(model.predict(data)[0])
     return {"res" : y_pred, "error_msg": ""}
 if __name__ == "__main__":
--- src/unit_test.py
+++ src/unit_test.py
@@ -2,26 +2,34 @@ import os
 import dotenv
 import mlflow
 import requests
-dotenv.load_dotenv(".env")
+import json
+import numpy as np
+dotenv.load_dotenv("../.env")
 def test_model_availability():
     # Arrange
     MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
     CHOSEN_MODEL = os.getenv("MODEL_ALIAS")
-    mlflow.set_tracking_uri(uri = MLFLOW_TRACKING_URI)
+    mlflow.set_tracking_uri(uri = "http://localhost:5000") 
     mlflow.set_experiment("Churn Experiment")
     
     # Act
-    chosen_model = mlflow.pyfunc.load_model(f"models:/RF best param@{CHOSEN_MODEL}")
+    chosen_model = mlflow.pyfunc.load_model(f"models:/RandomForest@{CHOSEN_MODEL}")
     # Assert
     assert type(chosen_model) == mlflow.pyfunc.PyFuncModel
 def test_predict_on_ci():
     # Arrange
     MODEL_SERVER_IP = os.getenv("MODEL_SERVER_IP")
-    data = {"x1": -0.3262214220367463, "x2": 0, "x3": 0, "x4": 0.2935174228967471, "x5": 2, "x6": -1.2258476714090278,"x7": 1,"x8": 1,"x9": 1,"x10": 101348.88}
+    data = {"CreditScore": -0.3262214220367463, "Geography": 0.0, "Gender": 0.0, "Age": 0.2935174228967471, "Tenure": 2.0, "Balance": -1.2258476714090278,"NumOfProducts": 1.0,"HasCrCard": 1.0,"IsActiveMember": 1.0,"EstimatedSalary": 101348.88}
+    #api_data = {"CreditScore": np.double(-0.3262214220367463), "Geography": np.double(0.0), "Gender": np.double(0.0), "Age": np.double(0.2935174228967471), "Tenure": np.double(2.0), "Balance": np.double(-1.2258476714090278),"NumOfProducts": np.double(1.0),"HasCrCard": np.double(1.0),"IsActiveMember": np.double(1.0),"EstimatedSalary": np.double(101348.88)}
+
     # Act
-    res = requests.post(MODEL_SERVER_IP, json = data)
-    # Assert
+    res = requests.post("http://localhost:8080/predict/", json = data)
+    #print(res.status_code)
+    #print(res.text)
+    #Assert
     assert res.status_code == 200
+test_model_availability()
+test_predict_on_ci()
 
 
 

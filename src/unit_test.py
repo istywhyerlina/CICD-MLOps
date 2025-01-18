e90462642f363899884efad56286faed8f69dc81
istywhyerlina <istywahyuerlina@gmail.com>
2025-01-18 13:43:07 +0700

    edit unit_test.py

--- src/unit_test.py
+++ src/unit_test.py
@@ -8,7 +8,9 @@ dotenv.load_dotenv("../.env")
 def test_model_availability():
     # Arrange
     MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+    print(MLFLOW_TRACKING_URI)
     CHOSEN_MODEL = os.getenv("MODEL_ALIAS")
+    print(CHOSEN_MODEL)
     mlflow.set_tracking_uri(uri = MLFLOW_TRACKING_URI ) 
     mlflow.set_experiment("Churn Experiment")
     
@@ -19,6 +21,7 @@ def test_model_availability():
 def test_predict_on_ci():
     # Arrange
     MODEL_SERVER_IP = os.getenv("MODEL_SERVER_IP")
+    print (MODEL_SERVER_IP)
     data = {"CreditScore": -0.3262214220367463, "Geography": 0.0, "Gender": 0.0, "Age": 0.2935174228967471, "Tenure": 2.0, "Balance": -1.2258476714090278,"NumOfProducts": 1.0,"HasCrCard": 1.0,"IsActiveMember": 1.0,"EstimatedSalary": 101348.88}
 
     # Act

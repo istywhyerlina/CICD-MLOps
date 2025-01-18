4558c00f408bc5a99c2ab56c38574697631c037d
istywhyerlina <istywahyuerlina@gmail.com>
2025-01-18 14:02:31 +0700

    edit unit_test.py

--- src/unit_test.py
+++ src/unit_test.py
@@ -4,13 +4,11 @@ import mlflow
 import requests
 import json
 import numpy as np
-dotenv.load_dotenv("../.env")
+dotenv.load_dotenv(".env")
 def test_model_availability():
     # Arrange
     MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
-    print(MLFLOW_TRACKING_URI)
     CHOSEN_MODEL = os.getenv("MODEL_ALIAS")
-    print(CHOSEN_MODEL)
     mlflow.set_tracking_uri(uri = MLFLOW_TRACKING_URI ) 
     mlflow.set_experiment("Churn Experiment")
     
@@ -25,10 +23,10 @@ def test_predict_on_ci():
     data = {"CreditScore": -0.3262214220367463, "Geography": 0.0, "Gender": 0.0, "Age": 0.2935174228967471, "Tenure": 2.0, "Balance": -1.2258476714090278,"NumOfProducts": 1.0,"HasCrCard": 1.0,"IsActiveMember": 1.0,"EstimatedSalary": 101348.88}
 
     # Act
-    #res = requests.post("http://localhost:8080/predict/", json = data)
+    res = requests.post("http://localhost:8080/predict/", json = data)
 
     #Assert
-    #assert res.status_code == 200
+    assert res.status_code == 200
 
 
 
